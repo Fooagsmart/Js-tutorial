061fc5d1ab5a673f239001d1a606c3990c326841
Oluwaseun <adunmofestus@gmail.com>
2023-09-21 00:20:41 +0100

    decimal

--- main.js
+++ main.js
@@ -12,3 +12,9 @@ let carName1 = "Volvo xc60";
 let carName2 = 'Volvo xc60';
 document.getElementById("den").innerHTML = carName1 + "<br>" + carName2;
 
+//<p>Numbers can be written with, or without decimals:</p>
+let x1 = 34.00;
+let x2 = 34;
+let x3 = 3.14;
+document.getElementById("o").innerHTML = x1  + "<br>" + x2 + "<br>" + x3;
+
