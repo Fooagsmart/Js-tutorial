29e853a11c0d52a3a27f12c6d12e85ece446e297
Oluwaseun <adunmofestus@gmail.com>
2023-09-21 00:08:31 +0100

    strings js

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 let x = 16 + "Volvo";
 document.getElementById("demo").innerHTML = x;
 
+//The + operator concatenates (adds) strings.
 let text1 = "John";
 let text2 = "Doe";
 let text3 = text1 + " " + text2;
 document.getElementById("dem").innerHTML = text3;
+
+//<p>Strings are written with quotes. You can use single or double quotes:</p>
+let carName1 = "Volvo xc60";
+let carName2 = 'Volvo xc60';
+document.getElementById("den").innerHTML = carName1 + "<br>" + carName2;
+
