3bbb2d9d1893710bce6929ede2069fcddefb343c
Oluwaseun <adunmofestus@gmail.com>
2023-09-20 23:42:29 +0100

    Js-tutorial

---
+++ main.js
@@ -0,0 +1,7 @@
+let x = 16 + "Volvo";
+document.getElementById("demo").innerHTML = x;
+
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+document.getElementById("dem").innerHTML = text3;
